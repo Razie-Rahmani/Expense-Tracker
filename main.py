7471bd6ec31bafc282cb9d6c159dc44099dc6e9f
Razie Rahmani <razz21700@gmail.com>
2026-08-05 06:40:46 -0300

    added add_row()

--- main.py
+++ main.py
@@ -11,16 +11,18 @@ import sqlite3
 from sqlite3 import Row
 
 database= sqlite3.connect("expense.sqlite")
-database.row_factory= sqlite3.Row
 cursor= database.cursor()
+database.row_factory= sqlite3.Row
 cursor.execute(""" 
-    CREATE TABLE IF NOT EXISTS "2026"(
+    CREATE TABLE IF NOT EXISTS Year_2026(
+        ID INTEGER PRIMARY KEY,
         Type TEXT,
         Amount INTEGER,
         Date TEXT
     )
 """)
-while True:
+
+def add_row():
     while True:
         transaction_type= input ("expense or income?")
         if transaction_type in ("expense", "income"):
@@ -29,24 +31,48 @@ while True:
             print("Invalid input. Please enter exactly 'expense' or 'income'.")
             continue
     while True:
-        amount_str= input ("how much?")
+        transaction_amount= input("How much?\n\n(Absolute Value)")
         try:
-            amount= int(amount_str)
+            amount= int(transaction_amount)
             break
         except ValueError:
             print("Invalid input. Please enter a valid integer amount.")
+            continue
     while True:
-        date= input ("date? (YYYY-MM-DD)")
+        transaction_date= input("What date?\n\n(YYYY-MM-DD)")
         try:
-            dt.strptime(date, "%Y-%m-%d")
+            dt.strptime(transaction_date, "%Y-%m-%d")
             break
         except ValueError:
             print("Invalid date format. Please use YYYY-MM-DD.")
             continue
     cursor.execute("""
-        INSERT INTO "2026"(Type, Amount, Date) VALUES(?, ?, ?)
-    """, (transaction_type, amount, date))
-    another= input ("add another?y/n")
+        INSERT INTO Year_2026(Type, Amount, Date) VALUES(?, ?, ?)
+        """, (transaction_type, amount, transaction_date))
     database.commit()
-    if another == "n":
-        break
+
+
+def edit_row():
+    pass
+
+def delete_row():
+    pass
+
+def sort():
+    pass
+
+def show_table():
+    pass
+
+while True:
+    user_request= input("What would you like to do?\n\nAdd, Edit, Delete, Sort, Show Table [Case Sensitive]")
+    if user_request== "Add":
+        add_row()
+    elif user_request== "Edit":
+        edit_row()
+    elif user_request== "Delete":
+        delete_row()
+    elif user_request== "Sort":
+        sort()
+    elif user_request== "Show Table":
+        show_table()
